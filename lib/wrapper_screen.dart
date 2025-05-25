cd024bb56b9fc8384b2d7be0b7c1487130e44334
Appvora <itappvora@gmail.com>
2025-05-25 16:49:15 +0530

    first commit

---
+++ lib/wrapper_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WrapperScreen extends StatefulWidget {
+  const WrapperScreen({super.key});
+
+  @override
+  State<WrapperScreen> createState() => _WrapperScreenState();
+}
+
+class _WrapperScreenState extends State<WrapperScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
